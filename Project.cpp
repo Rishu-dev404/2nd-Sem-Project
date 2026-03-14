ee0c992afae73d6c2d0d9c98392f2c33377336ae
Rishuchakravarty55 <rishuchakravorty936@gmail.com>
2026-03-14 11:19:57 +0530

    feat: add restaurant menu display with 19 items

--- Project.cpp
+++ Project.cpp
@@ -3,19 +3,19 @@
 using namespace std;
 
 // Menu data
-string itemNames[5] = {"Burger", "Pizza", "Pasta", "Cold Drink", "Ice Cream"};
-float itemPrices[5] = {50, 120, 90, 30, 40};
+string itemNames[19] = {"Burger", "Pizza", "Pasta", "Cold Drink", "Ice Cream", "Chaowmin", "Garlic Bread", "Tacos", "Spring Roll", "Momos", "Choco Shake", "Mango Shake", "Cold Coffee", "Oreo Shake", "Samosa", "Dosa", "Idli", "Vada", "Pav Bhaji"};
+float itemPrices[19] = {70, 150, 90, 30, 40, 60, 65, 65, 40, 80, 65, 40, 55, 70, 30, 65, 50, 45, 60};
 
 // Function to display menu
-void displayMenu()
-{
-    cout << "===== RESTAURANT MENU =====" << endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout << i + 1 << ". " << itemNames[i]
-             << " - Rs." << itemPrices[i] << endl;
+void displayMenu() {
+    cout << "==============================" << endl;
+    cout << "       RESTAURANT MENU        " << endl;
+    cout << "==============================" << endl;
+    for (int i = 0; i < 19; i++) {
+        cout << i+1 << ". " << itemNames[i]
+             << "\t\t Rs." << itemPrices[i] << endl;
     }
-    cout << "===========================" << endl;
+    cout << "==============================" << endl;
 }
 
 int main()
